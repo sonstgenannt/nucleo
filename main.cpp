77069deee504450b02e2d3eeb4d21cfd286e6d5f
sonstgenannt <jdsf2003@gmail.com>
2022-11-25 11:37:09 +0000

    fixed version of code

--- main.cpp
+++ main.cpp
@@ -9,5 +9,17 @@ int main()
   float meas_r;
   float meas_v;
 
+  mypwm.period_ms(10);
+  mypwm.pulsewidth_ms(1);
 
+  while(1)
+  {
+    meas_r = analog_value.read();
+
+    mypwm.write(meas_r);
+
+    printf("pwm set to %.2f %%\n", mypwm.read() * 100);
+
+    wait(1.0);
+  }
 }
